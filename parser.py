713282ad3e9f0e73178b96df2a4cc21c51792f65
fattouche <alex.fattouche@gmail.com>
2018-07-06 16:53:48 -0700

    Fix parser

--- parser.py
+++ parser.py
@@ -2,20 +2,23 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('data/input.xml')
 data = tree.getroot()
 
+# Students and classes
+students = {}
+classes = {}
+for raw_student in data[3]:
+    student_id = int(raw_student.attrib["id"])
+    students[student_id] = []
+    for raw_class in raw_student:
+        class_id = int(raw_class.attrib["id"])
+        students[student_id].append(class_id)
+        if(class_id not in classes):
+            classes[class_id] = []
+        classes[class_id].append(student_id)
+
 # Rooms
 rooms = {}
 for room in data[0]:
-    rooms[room.attrib["id"]] = {
-        "capacity": room.attrib["capacity"], "location": room.attrib["location"]}
-
-# classes
-classes = []
-for class_id in data[1]:
-    classes.append(class_id.attrib["id"])
-
-# Students
-students = {}
-for student in data[3]:
-    students[student.attrib["id"]] = []
-    for class_id in student:
-        students[student.attrib["id"]].append(class_id.attrib["id"])
+    location_x = int(room.attrib["location"].split(",")[0])
+    location_y = int(room.attrib["location"].split(",")[1])
+    rooms[int(room.attrib["id"])] = {
+        "capacity": int(room.attrib["capacity"]), "location_x": location_x, "location_y": location_y}
