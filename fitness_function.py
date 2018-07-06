097a23cf428c1326d7b3afdaad98cce8e1cd26ed
Cailan St Martin <cailan@marinelabs.io>
2018-07-06 15:31:56 -0700

    Add shell fitness function

---
+++ fitness_function.py
@@ -0,0 +1,17 @@
+def calc_num_student_course_conflicts(chromosome, student_courses_dict):
+  pass
+
+def calc_num_course_time_loc_conflicts(chromosome):
+  pass
+
+def calc_num_overfilled_rooms(chromosome, room_information_dict):
+  pass
+
+def calc_num_unfavoured_time_slots_used(chromosome):
+  pass
+
+def calc_num_empty_seats_in_course_rooms(chromosome):
+  pass
+
+def calc_fitness(chromosome):
+  pass
