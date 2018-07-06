3b380634494855d3ad05ec097540d3604577296a
Cailan St Martin <cailan@marinelabs.io>
2018-07-06 16:24:53 -0700

    Update to consistently use timeslot over time_slot

--- fitness_function.py
+++ fitness_function.py
@@ -1,5 +1,5 @@
 ALLELE_ROOM_ID_INDEX = 0
-ALLELE_TIME_SLOT_INDEX = 1
+ALLELE_TIMESLOT_INDEX = 1
 ALLELE_DAYS_INDEX = 2
 PREFERRED_START = 4
 PREFERRED_END = 16
@@ -11,22 +11,22 @@ def calc_num_student_course_conflicts(chromosome, student_courses_dict):
     student_courses = student_courses_dict[student]
 
     # Get time slots of each course from the chromosome
-    course_time_slots = [
-      chromosome[course][ALLELE_TIME_SLOT_INDEX] for course in student_courses
+    course_timeslots = [
+      chromosome[course][ALLELE_TIMESLOT_INDEX] for course in student_courses
     ] 
 
     # Sort the courses by start time
     # This is O(1) since number of courses a student can have is O(1)
-    course_time_slots.sort(
-      Key=lambda time_slot: time_slot[0]
+    course_timeslots.sort(
+      Key=lambda timeslot: timeslot[0]
     )
 
-    for i in range(1, len(course_time_slots)):
-      prev_time_slot = course_time_slots[i-1]
-      curr_time_slot = course_time_slots[i]
+    for i in range(1, len(course_timeslots)):
+      prev_timeslot = course_timeslots[i-1]
+      curr_timeslot = course_timeslots[i]
 
-      prev_end_time = prev_time_slot[1]
-      curr_start_time = curr_time_slot[0]
+      prev_end_time = prev_timeslot[1]
+      curr_start_time = curr_timeslot[0]
 
       if (curr_start_time <= prev_end_time):
         num_student_course_conflicts += 1
@@ -40,12 +40,12 @@ def calc_num_overfilled_rooms(chromosome, room_information_dict):
   pass
 
 
-def calc_num_unfavoured_time_slots_used(chromosome):
+def calc_num_unfavoured_timeslots_used(chromosome):
   num_unfavoureds_slots = 0
   
   for class_id in chromosome:
-    start_timeslot = chromosome[class_id][ALLELE_TIME_SLOT_INDEX][0]
-    end_timeslot = chromosome[class_id][ALLELE_TIME_SLOT_INDEX][1]
+    start_timeslot = chromosome[class_id][ALLELE_TIMESLOT_INDEX][0]
+    end_timeslot = chromosome[class_id][ALLELE_TIMESLOT_INDEX][1]
 
     if start_timeslot < PREFERRED_START or end_timeslot > PREFERRED_END:
       num_unfavoureds_slots +=1
