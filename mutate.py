364f177e1511943487be022d1e04c639c994a3c2
fattouche <alex.fattouche@gmail.com>
2018-07-07 19:00:42 -0700

    Fix

--- mutate.py
+++ mutate.py
@@ -3,7 +3,7 @@ from random import sample
 
 
 def mutate(parent):
-    classes = sample(range(len(parent)), randint(1, len(parent)//2))
+    classes = sample(range(1, len(parent)), randint(1, len(parent)//2))
     # Run when generate exists
     # for i in classes:
     #     parent[i] = generate()
