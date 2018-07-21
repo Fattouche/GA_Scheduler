0af19cef6de74d71651e7bad9d48366ca3d260a1
CailanStm <cailanstm@shaw.ca>
2018-07-21 12:26:56 -0700

    Update parser test based on new interface

--- Tests/parser_test.py
+++ Tests/parser_test.py
@@ -8,16 +8,21 @@ sys.path.append("..")
 import generator as gen
 import mutate as mutator
 import crossover as crosser
-import xml_parser as parser
+from xml_parser import Parser
 
 
 MAX_TIMESLOT = 27
 
 
 class TestingParser(unittest.TestCase):
+    def setUp(self):
+        self.input_file = '../data/purdue_math_dataset/input.xml'
+
     def test_parser(self):
-        classes = parser.get_classes()
-        students = parser.get_students()
+        parser = Parser(self.input_file)
+
+        rooms, classes, students, class_map = parser.parse_input()
+
         for class_id in classes:
             students_in_class = classes[class_id]
             for student in students_in_class:
