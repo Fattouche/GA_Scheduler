d1f5741d21e63d1ae604fd3fb2d92c220c47b650
CailanStm <cailanstm@shaw.ca>
2018-07-21 20:36:05 -0700

    Optimize fitness function, so that it iterates over each chromosome only once in order to calculate fitness
    - Also go through every fit-enough individual to try to find a valid
    solution, rather than only the fittest one

--- fitness_function.py
+++ fitness_function.py
@@ -50,74 +50,102 @@ class FitnessCalculator:
                         num_student_course_conflicts += 1
 
         return num_student_course_conflicts
+            
 
+    def calc_single_course_room_time_conflicts(self, chromosome, course_id, 
+        courses_per_time_and_day):
 
-    def calc_num_course_room_time_conflicts(self, chromosome):
         num_course_room_time_conflicts = 0
 
-        # Index this dict by (time, day)
-        courses_per_time_and_day = defaultdict(set)
-        for course_id in chromosome:
-            course_info_list = chromosome[course_id]
-            room_id = course_info_list[COURSE_INFO_ROOM_ID_INDEX]
+        course_info_list = chromosome[course_id]
+        room_id = course_info_list[COURSE_INFO_ROOM_ID_INDEX]
 
-            for time in range(course_info_list[COURSE_INFO_TIMESLOT_INDEX][0],
-                            course_info_list[COURSE_INFO_TIMESLOT_INDEX][1] + 1):
-                for day in course_info_list[COURSE_INFO_DAYS_INDEX]:
-                    if room_id in courses_per_time_and_day[(time, day)]:
-                        num_course_room_time_conflicts += 1
-                    else:
-                        courses_per_time_and_day[(time, day)].add(room_id)
+        for time in range(course_info_list[COURSE_INFO_TIMESLOT_INDEX][0],
+                        course_info_list[COURSE_INFO_TIMESLOT_INDEX][1] + 1):
+            for day in course_info_list[COURSE_INFO_DAYS_INDEX]:
+                if room_id in courses_per_time_and_day[(time, day)]:
+                    num_course_room_time_conflicts += 1
+                else:
+                    courses_per_time_and_day[(time, day)].add(room_id)
 
         return num_course_room_time_conflicts
 
 
-    def calc_total_room_overflow(self, chromosome):
-        total_room_overflow = 0
+    def calc_single_room_overflow(self, chromosome, course_id):
+        room_overflow = 0
 
-        for course in chromosome:
-            course_info_list = chromosome[course]
-            room_id = course_info_list[COURSE_INFO_ROOM_ID_INDEX]
+        course_info_list = chromosome[course_id]
+        room_id = course_info_list[COURSE_INFO_ROOM_ID_INDEX]
 
-            room_capacity = self.room_information_dict[room_id]['capacity']
-            num_students_in_course = len(self.course_students_dict[course])
+        room_capacity = self.room_information_dict[room_id]['capacity']
+        num_students_in_course = len(self.course_students_dict[course_id])
 
-            if (num_students_in_course > room_capacity):
-                total_room_overflow += (num_students_in_course - room_capacity)
+        if (num_students_in_course > room_capacity):
+            room_overflow += (num_students_in_course - room_capacity)
 
-        return total_room_overflow
+        return room_overflow
 
 
-    def calc_num_unfavoured_timeslots_used(self, chromosome):
+    def calc_single_course_unfavoured_timeslots(self, chromosome, course_id):
         num_unfavoureds_slots = 0
 
-        for class_id in chromosome:
-            for time in range(chromosome[class_id][COURSE_INFO_TIMESLOT_INDEX][0],
-                            chromosome[class_id][COURSE_INFO_TIMESLOT_INDEX][1] + 1):
+        for time in range(chromosome[course_id][COURSE_INFO_TIMESLOT_INDEX][0],
+                            chromosome[course_id][COURSE_INFO_TIMESLOT_INDEX][1] + 1):
 
-                if time < PREFERRED_START or time > PREFERRED_END:
-                    num_unfavoureds_slots += 1
+            if time < PREFERRED_START or time > PREFERRED_END:
+                num_unfavoureds_slots += 1
+
         return num_unfavoureds_slots
 
 
-    def calc_num_empty_seats_in_course_rooms(self, chromosome):
+    def calc_num_empty_seats_single_course(self, chromosome, course_id):
         num_empty_seats = 0
-        if(self.calc_total_room_overflow(chromosome) > 0):
-            return num_empty_seats
-        for class_id in chromosome:
-            capacity = self.room_information_dict[chromosome[class_id][0]]["capacity"]
-            enrolled = len(self.course_students_dict[class_id])
+
+        capacity = self.room_information_dict[chromosome[course_id][0]]["capacity"]
+        enrolled = len(self.course_students_dict[course_id])
+
+        if capacity > enrolled and enrolled != 0:
             num_empty_seats += (capacity - enrolled)
+
         return num_empty_seats
 
 
+    def iterate_over_chromosome(self, chromosome):
+        num_course_room_time_conflicts = 0
+        total_room_overflow = 0
+        num_unfavoureds_slots = 0
+        num_empty_seats = 0
+
+        # Index this dict by (time, day) - for calculating number of course
+        # room time conflicts
+        courses_per_time_and_day = defaultdict(set)
+
+        for course_id in chromosome:
+            num_course_room_time_conflicts += self.calc_single_course_room_time_conflicts(
+                chromosome, course_id, courses_per_time_and_day)
+                
+            total_room_overflow += self.calc_single_room_overflow(chromosome, course_id)
+            
+            num_unfavoureds_slots += self.calc_single_course_unfavoured_timeslots(chromosome, course_id)
+            
+            num_empty_seats += self.calc_num_empty_seats_single_course(chromosome, course_id)
+
+        return (num_course_room_time_conflicts, total_room_overflow,
+                num_unfavoureds_slots, num_empty_seats)
+
+
+
     def calc_fitness(self, chromosome):
+        num_course_room_time_conflicts, total_room_overflow, \
+        num_unfavoureds_slots, num_empty_seats = \
+            self.iterate_over_chromosome(chromosome)
+
         fitness = \
             1.0 * self.calc_num_student_course_conflicts(chromosome) \
-            + 1.0 * self.calc_num_course_room_time_conflicts(chromosome) \
-            + 1.0 * self.calc_total_room_overflow(chromosome) \
-            + 0.1 * self.calc_num_unfavoured_timeslots_used(chromosome) \
-            + 0.05 * self.calc_num_empty_seats_in_course_rooms(chromosome)
+            + 1.0 * num_course_room_time_conflicts \
+            + 1.0 * total_room_overflow \
+            + 0.1 * num_unfavoureds_slots \
+            + 0.05 * num_empty_seats
 
         return fitness
 
@@ -126,18 +154,23 @@ class FitnessCalculator:
         if chromosome is None:
             return False
         student_conflicts = self.calc_num_student_course_conflicts(chromosome)
-        room_conflicts = self.calc_num_course_room_time_conflicts(chromosome)
-        room_overflow = self.calc_total_room_overflow(chromosome)
+
+        room_conflicts, room_overflow, _, _ = self.iterate_over_chromosome(chromosome)
 
         return True if (student_conflicts == 0 and room_conflicts == 0 and room_overflow == 0) else False
 
 
-    def fittest_chromosome(self, population):
-        min_fitness = self.fitness_threshold
-        fittest_chromosome = None
+    def get_fit_chromosomes(self, population):
+        fit_chromosomes = []
+        min_fitness = float('inf')
+
         for chromosome in population:
             chromosome_fitness = self.calc_fitness(chromosome)
-            if chromosome_fitness < min_fitness:
-                fittest_chromosome = chromosome
-                min_fitness = chromosome_fitness
-        return fittest_chromosome
+            if chromosome_fitness <= self.fitness_threshold:
+                fit_chromosomes.append((chromosome, chromosome_fitness))
+           
+            min_fitness = min(min_fitness, chromosome_fitness)
+
+        print('Min fitness: {}'.format(min_fitness))
+
+        return fit_chromosomes
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -12,7 +12,10 @@ POPULATION_SIZE = 100
 parser = argparse.ArgumentParser(description='Genetic algorithm for developing a timetable schedule.')
 parser.add_argument('input_file', type=str,
                     help='XML file that defines classes, students and rooms.')
-parser.add_argument('fitness_threshold', type=int, help='Fitness value below which chromosomes will be accepted (want to minimize fitness value).')
+parser.add_argument('fitness_threshold', type=float, help='Fitness value below which chromosomes will be accepted (want to minimize fitness value).')
+parser.add_argument('--round_limit', type=int, default=0, help='Maximum number of selection rounds to\
+                        execute. If set to 0, will run indefinitely (or until a\
+                        solution is reached.')
 
 args = parser.parse_args()
 
@@ -25,7 +28,7 @@ def seed_population(population_size, course_students_dict, room_ids):
     return population
 
 
-def preform_genetic_algorithm(input_file, fitness_threshold):
+def preform_genetic_algorithm(input_file, fitness_threshold, round_limit):
     parser = Parser(input_file)
 
     room_information_dict, course_students_dict, student_courses_dict, _ = parser.parse_input()
@@ -37,19 +40,61 @@ def preform_genetic_algorithm(input_file, fitness_threshold):
     fitness_calculator = FitnessCalculator(room_information_dict, course_students_dict,
                                             student_courses_dict, fitness_threshold)
 
-    while(True):
+    rounds = 1
+    min_fitness = float('inf')
+    while rounds <= round_limit or round_limit == 0:
         print("Beginning new selection")
         population = selector.selection(population, fitness_calculator)
-        population = crossover.population_crossover(population)
-        population = mutator.mutate_population(population, room_ids)
-        fittest_chromosome = fitness_calculator.fittest_chromosome(population)
 
-        if fitness_calculator.is_valid(fittest_chromosome):
-            return fittest_chromosome, fitness_calculator.calc_fitness(fittest_chromosome)
+        # print('population after selection:')
+        # for individual in population:
+        #     print(individual)
+        #     print('***')
+        # print('end population')
+
+        population = crossover.population_crossover(population)
+
+        # print('population after crossover:')
+        # for individual in population:
+        #     print(individual)
+        #     print('***')
+        # print('end population')
+
+        population = mutator.mutate_population(population, room_ids)
+
+        # print('population after mutation:')
+        # for individual in population:
+        #     print(individual)
+        #     print('***')
+        # print('end population')
+
+        fit_chromosomes = fitness_calculator.get_fit_chromosomes(population)
+
+        # distinct_individuals = []
+        # for individual in population:
+        #     if individual not in distinct_individuals:
+        #         distinct_individuals.append(individual)
+        # print('Num distinct individuals after mutation: {}'.format(len(distinct_individuals)))
+
+        if fit_chromosomes:
+            print('Num fit chromosomes: {}'.format(len(fit_chromosomes)))
+
+        for chromosome, fitness in fit_chromosomes:
+            min_fitness = min(min_fitness, fitness)
+            if fitness_calculator.is_valid(chromosome):
+                return chromosome, fitness, rounds
+
+        rounds += 1
+
+    print('Failed. Min fitness found: {}'.format(min_fitness))
     return None
 
 
 if __name__ == "__main__":
-    chromosome = preform_genetic_algorithm(args.input_file, args.fitness_threshold)
+    result = preform_genetic_algorithm(args.input_file, args.fitness_threshold,
+        args.round_limit)
     print("Finished calculating")
-    print(chromosome)
+    if result:
+        print('chromosome: {}'.format(result[0]))
+        print('fitness: {}'.format(result[1]))
+        print('rounds: {}'.format(result[2]))
